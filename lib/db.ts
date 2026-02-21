3a1af44125fee9aff7128fad83c5ed040635edff
Your Name <you@example.com>
2026-02-21 13:16:59 -0600

    generate prisma client

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,3 @@
-import { PrismaClient } from '@prisma/client';
+import { prisma } from './prisma';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-export const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+export { prisma };
